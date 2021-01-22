ab82e6bdcd48217e2d936c9e8ca083ee81f842ef
Yordan Stoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-22 15:19:46 +0200

    Add files via upload

---
+++ Classroom/ClassroomProject.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassroomProject
+{
+    public class Classroom
+    {
+        private List<Student> students;
+        public Classroom(int capacity)
+        {
+            this.Capacity = capacity;
+            students = new List<Student>();
+        }
+        public int Capacity { get; set; }
+        public int Count { get; set; }
+
+        public void RegisterStudent(Student student)
+        {
+            if (students.Count <= Capacity)
+            {
+                students.Add(student);
+                Console.WriteLine($"Added student {student.FirstName} {student.LastName}");
+            }
+            else
+            {
+                Console.WriteLine("No seats in the classroom");
+            }
+        }
+        public bool DismissStudent(Student student, string firstName, string lastName)
+        {
+            Student studentToRemove = students.FirstOrDefault(
+                s => s.FirstName == firstName 
+            && s.LastName == lastName);
+            if (studentToRemove != null)
+            {
+                students.Remove(studentToRemove);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Student not found");
+            }
+            return false;
+        }
+
+        public string GetSubjectInfo(string subject, string firstName, string lastName)
+        {
+            if (students.Count() != 0)
+            {
+                return $"Subject: {subject} Students: {firstName} {lastName} ";
+            }
+            else
+            {
+                return "No students enrolled for the subject";
+            }
+        }
+        
+        public void GetStudent(string firstName, string lastName)
+        {
+            Console.WriteLine($"{firstName} {lastName}");
+        }
+    }
+}
---
+++ Classroom/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassroomProject
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            // Initialize the repository
+            Classroom classroom = new Classroom(10);
+            // Initialize entities
+            Student student = new Student("Peter", "Parker", "Geometry");
+            Student studentTwo = new Student("Sarah", "Smith", "Algebra");
+            Student studentThree = new Student("Sam", "Winchester", "Algebra");
+            Student studentFour = new Student("Dean", "Winchester", "Music");
+            // Print Student
+            Console.WriteLine(student); // Student: First Name = Peter, Last Name = Parker, Subject = Geometry
+                                        // Register Student
+            
+            classroom.RegisterStudent(student);
+             // Added student Peter Parker
+            classroom.RegisterStudent(studentTwo);
+            classroom.RegisterStudent(studentThree);
+            classroom.RegisterStudent(studentFour);
+            // Dismiss Student
+            classroom.DismissStudent(null, "Peter", "Parker");
+            
+            classroom.DismissStudent(null, "Ellie", "Goulding");
+            // Student not found
+                                             // Subject info
+            classroom.GetSubjectInfo(null, "Algebra", null);
+            
+            // Subject: Algebra
+            // Students:
+            // Sarah Smith
+            // Sam Winchester
+            classroom.GetSubjectInfo("Art", null, null);
+            // No students enrolled for the subject
+                                            // Get Student
+            //Console.WriteLine(classroom.GetStudent("Dean", "Winchester"));
+            // Student: First Name = Dean, Last Name = Winchester, Subject = Music
+
+        }
+    }
+}
