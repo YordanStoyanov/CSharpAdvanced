e6c2585691de9994c19d07af6b753efc734620bc
Yordan Stoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-22 14:07:00 +0200

    Add files via upload

---
+++ VetClinic/Clinic.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace VetClinic
+{
+    public class Clinic
+    {
+        private int capacity;
+        private List<Pet> data;
+
+        public Clinic(int capasity)
+        {
+            this.Capacity = capasity;
+            data = new List<Pet>();
+        }
+
+        public int Capacity { get; set; }
+
+        public void Add(Pet pet)
+        {
+            data.Add(pet);
+        }
+
+        public bool Remove(string name)
+        {
+            Pet pet = data.FirstOrDefault(p => p.Name == name);
+            if (pet != null)
+            {
+                data.Remove(pet);
+                return true;
+            }
+            return false;
+        }
+
+        public void GetPet(string name, string owner)
+        {
+            Pet pet = data.FirstOrDefault(p => p.Name == name && p.Owner == owner);
+        }
+
+        public void GetOldestPet(int age)
+        {
+            Pet pet = data.FirstOrDefault(p => p.Age == age);
+
+        }
+        
+    }
+}
