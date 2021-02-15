55a6b85cf3ed4e06a50ddd761e7dd9eeb892ecae
Yordan Stoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-02-15 20:12:30 +0200

    Add files via upload

---
+++ StacksAndQueuesExercise/BasicQueueOperations/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BasicQueueOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            //int numbers = input[0];
+            int dequeueElements = input[1];
+            int findElement = input[2];
+
+            Queue<int> queue = new Queue<int>(numbers);
+
+            for (int i = 0; i < dequeueElements; i++)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Contains(findElement))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                int minNumberInStack = queue.Min();
+                Console.WriteLine(minNumberInStack);
+            }
+        }
+    }
+}
---
+++ StacksAndQueuesExercise/BasicStackOperations/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BasicStackOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            //int numbers = input[0];
+            int popElements = input[1];
+            int findElement = input[2];
+
+            Stack<int> stack = new Stack<int>(numbers);
+
+            for (int i = 0; i < popElements; i++)
+            {
+                stack.Pop();
+            }
+
+            if (stack.Contains(findElement))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                int minNumberInStack = stack.Min();
+                Console.WriteLine(minNumberInStack);
+            }
+        }
+    }
+}
