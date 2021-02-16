5aeccd237590139354338078592ef030605e1ea8
Yordan Stoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-02-16 11:41:36 +0200

    Add files via upload

---
+++ Guild/Guild.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Guild
+{
+    public class Guild
+    {
+        private List<Player> roster;
+        public Guild(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.roster = new List<Player>();
+        }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+
+        public int Count { get; }
+
+        public void AddPlayer(Player player)
+        {
+            if (this.roster.Count < this.Capacity)
+            {
+                this.roster.Add(player);
+            }
+        }
+
+        public bool RemovePlayer(string name)
+        {
+            Player player = roster.FirstOrDefault(p => p.Name == name);
+            if (player != null)
+            {
+                this.roster.Remove(player);
+                return true;
+            }
+            return false;
+
+            //foreach (var player in roster)
+            //{
+            //    if (player.Name == name)
+            //    {
+            //        roster.Remove(player);
+            //        break;
+            //    }
+            //}
+        }
+        public Player PromotePlayer(string name)
+        {
+            Player player = roster.FirstOrDefault(p => p.Name == name);
+            if (player.Rank != "Member")
+            {
+                player.Rank = "Member";
+            }
+            return player;
+        }
+
+        public Player DemotePlayer(string name)
+        {
+            Player player = roster.FirstOrDefault(p => p.Name == name);
+            if (player.Rank != "Trial")
+            {
+                player.Rank = "Trial";
+            }
+            return player;
+        }
+
+        public Player[] KickPlayersByClass(string @class)
+        {
+            Player[] player =new Player[] {roster.FirstOrDefault(p => p.Class == @class)};
+            
+            if (player != null)
+            {
+                
+                this.roster.Remove(player.FirstOrDefault());
+            }
+            return player;
+        }
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Players in the guild: {Name}");
+            foreach (var player in roster)
+            {
+                sb.AppendLine($"{player.Name}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Guild/Player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guild
+{
+    public class Player
+    {
+        public Player(string name, string @class)
+        {
+            this.Name = name;
+            this.Class = @class;
+        }
+        
+        public Player(string name, string @class, string description)
+        {
+            this.Name = name;
+            this.Class = @class;
+            this.Description = "n/a";
+        }
+        public Player(string name, string @class, string rank, string description)
+        {
+            this.Name = name;
+            this.Class = @class;
+            this.Rank = "Trial";
+            this.Description = "n/a";
+        }
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string Rank { get; set; }
+        public string Description { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendLine($"Player {Name}: {Class}");
+            result.AppendLine($"Rank: {Rank}");
+            result.AppendLine($"Description: {Description}");
+            return result.ToString().TrimEnd();
+        }
+    }
+}
